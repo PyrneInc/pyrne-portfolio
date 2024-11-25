4abc89d3653f53a22f086cc411f011df4fbef1f8
MuriithiMark <markmwangi14317@gmail.com>
2024-11-25 06:45:37 +0300

    intial hero-section design

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/about/About";
 import ContactMe from "@/components/contact-me/ContactMe";
 import Header from "@/components/header/Header";
+import HeroSection from "@/components/hero-section/HeroSection";
 import Projects from "@/components/projects/Projects";
 import Skills from "@/components/skills/Skills";
 import Image from "next/image";
@@ -9,7 +10,8 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main id="#home">
+      <main className="p-4">
+        <HeroSection />
         <About />
         <Projects />
         <Skills />
---
+++ components/hero-section/HeroSection.tsx
@@ -0,0 +1,12 @@
+export default function HeroSection() {
+    return (
+        <div id="home">
+            <h1 className="text-white text-4xl font-bold">
+                Hi, I'm <span className="text-electricBlue">Mark Mwangi</span>
+            </h1>
+            <p className="text-metallicGrey mt-2">
+                A Software Enginner passionate about building innovative solutions.
+            </p>
+        </div>
+    )
+}
